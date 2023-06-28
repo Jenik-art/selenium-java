efb5aed06fb8728aa559f0cab70c4d44e8a1b787
Yauhen Artsiomenka <yauhena@playtika.com>
2023-06-28 13:53:29 +0300

    Second commit for selenium project. Find all links and click at 10th link.

--- selenium-java/src/test/java/OpenPageTest.java
+++ selenium-java/src/test/java/OpenPageTest.java
@@ -1,11 +1,18 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class OpenPageTest {
     @Test
     public static void clickOnLink(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com");
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        System.out.println("Total links on the Wb Page: " + allLinks.size());
+        allLinks.get(10).click();
     }
 }
